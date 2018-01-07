4ba9c6f2c33493c487354de81edc10a5276182bb
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-06 19:09:15 -0600

    getting setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }                from '@angular/platform-browser';
+import { NgModule }                     from '@angular/core';
 
+import { AppComponent }                 from './app.component';
 
-import { AppComponent } from './app.component';
-
+import { AngularFireModule }            from 'angularfire2';
+import { AngularFireDatabaseModule }    from 'angularfire2/database';
+import { AngularFireAuthModule }        from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB9xEU1aYS-otAkFuYkO-k8hPTbycBTbQk",
+    authDomain: "angularbootstrpfb.firebaseapp.com",
+    databaseURL: "https://angularbootstrpfb.firebaseio.com",
+    projectId: "angularbootstrpfb",
+    storageBucket: "",
+    messagingSenderId: "319018489547"
+  }
 };
